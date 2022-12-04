736b7332b760ca8c9f4cb88a6d8b4cd275ee6c54
sofotech <a.nijsten@live.nl>
2022-12-04 14:26:24 +0100

    Day 4

---
+++ day4.2.py
@@ -0,0 +1,11 @@
+with open("input4.txt", 'r') as f:
+    lines = f.readlines()
+    result = 0
+    for l in lines:
+        assignments = l[:-1].split(",")
+        elf1 = assignments[0].split("-")
+        elf2 = assignments[1].split("-")
+        if int(elf1[1]) >= int(elf2[0]) and int(elf1[0]) <= int(elf2[1]) or int(elf2[1]) >= int(elf1[0]) and int(elf2[0]) <= int(elf1[1]):
+            result += 1
+            
+print(result)
